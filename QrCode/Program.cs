94138fbc2b8298963509bb65b682804ea72e22d8
Farrush <rcarrenhot@gmail.com>
2022-06-24 09:39:10 -0300

    Commit

--- QrCode/Program.cs
+++ QrCode/Program.cs
@@ -17,7 +17,7 @@ namespace QrCode
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMenu());
+            Application.Run(new frmGerenciarProdutos());
         }
     }
 }
